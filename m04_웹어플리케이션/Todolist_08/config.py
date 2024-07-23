ae909195ca7989f57e903181c111f180bbab39b9
wjsghk1267 <167948294+wjsghk1267@users.noreply.github.com>
2024-07-23 17:40:15 +0900

    Delete m04_웹어플리케이션/Todolist_08 directory

--- m04_웹어플리케이션/Todolist_08/config.py
+++
@@ -1,4 +0,0 @@
-SECRET_KEY = '57a9c9d1f9aea862f8aaf1c667b0abab39bc3bcd78647d61'
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///tasks.db'
-SQLALCHEMY_DATABASE_URI = "mysql://wjsghk1267:Wjsghk1267!@localhost:3306/wjsghk1267_db"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
